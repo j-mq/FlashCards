2675db58d4b7dee741bd432b918d4e5314c78489
Jacobo <jacobo@fixel.co.jp>
2022-08-04 18:49:09 +0900

    basic layout done

--- flash-cards/src/constants/designTokens.ts
+++ flash-cards/src/constants/designTokens.ts
@@ -1,5 +1,11 @@
 const theme = {
-  backgroundPrimary: '#E9E0C5',
+  backgroundPrimaryMain: '#EAB10A',
+  backgroundPrimaryActive: '#E9E0C5',
+  backgroundPrimaryLight: '#CBB679',
+  backgroundPrimaryLightest: '#FBF9F5',
+  backgroundPrimaryDark: '#9F8330',
+  backgroundSecondaryMain: '#9F80FF',
+  backgroundSecondaryDark: '#3C00F5',
 };
 
 export default theme;
--- flash-cards/src/pages/CardsDisplay.tsx
+++ flash-cards/src/pages/CardsDisplay.tsx
@@ -2,11 +2,93 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CardsDisplayContainer = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  padding: 20px;
+  height: 100vh;
+  width: 100%;
+
+  @media (max-height: 844px) {
+    flex-direction: row;
     justify-content: center;
-    height: 100vh;
-    background: ${(props) => props.theme.backgroundPrimary};
+  }
+`;
+
+const Card = styled.div`
+  width: 350px;
+  height: 540px;
+  flex-shrink: 0;
+  background-color: ${(props) => props.theme.backgroundPrimaryLightest};
+  border-radius: 10px;
+`;
+
+const CardImage = styled.div`
+  width: 100%;
+  height: 63%;
+  background: ${(props) => props.theme.backgroundPrimaryLight};
+  border-radius: 10px 10px 0px 0px;
+`;
+
+const CardText = styled.div`
+  width: 100%;
+  height: 33%;
+  display: flex;
+  background: ${(props) => props.theme.backgroundPrimaryLightest};
+  padding: 20px;
+  border-radius: 10px 10px 0px 0px;
+  font-weight: 400;
+  font-size: 1.25rem;
+  line-height: 150%;
+  letter-spacing: 0.02em;
+`;
+
+const ActionsArea = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: 390px;
+  padding: 20px;
+  margin-top: 16px;
+
+  @media (max-height: 844px) {
+    flex-direction: column;
+    height: 540px;
+    margin-top: 0px;
+    margin-left: 16px;
+    padding: 0px 20px;
+  }
+`;
+
+const DrawButton = styled.button`
+  background: ${(props) => props.theme.backgroundPrimaryMain};
+  width: 150px;
+  height: 230px;
+  border-radius: 10px;
+  border: none;
+  color: white;
+  font-weight: 800;
+  font-size: 1.25rem;
+  cursor: pointer;
+  :active {
+    background: ${(props) => props.theme.backgroundPrimaryDark};
+    transform: scale(0.95);
+  }
+`;
+
+const ShuffleButton = styled.button`
+  background: none;
+  border: none;
+  color: white;
+  font-weight: 800;
+  font-size: 1.5rem;
+  cursor: pointer;
+  color: ${(props) => props.theme.backgroundSecondaryMain};
+  :active {
+    color: ${(props) => props.theme.backgroundSecondaryDark};
+    transform: scale(0.95);
+  }
 `;
 
 type CardsDisplayProps = {
@@ -16,7 +98,19 @@ type CardsDisplayProps = {
 const CardsDisplay: React.FC<CardsDisplayProps> = ({
   color,
 }: CardsDisplayProps) => (
-  <CardsDisplayContainer>CardsDisplay: {color}</CardsDisplayContainer>
+  <CardsDisplayContainer>
+    <Card>
+      <CardImage />
+      <CardText>
+        Lorem {color} ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </CardText>
+    </Card>
+    <ActionsArea>
+      <ShuffleButton>SHUFFLE</ShuffleButton>
+      <DrawButton>DRAW</DrawButton>
+    </ActionsArea>
+  </CardsDisplayContainer>
 );
 
 export default CardsDisplay;
